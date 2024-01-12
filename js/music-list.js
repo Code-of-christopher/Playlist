b75f06dc413b944eaa6862ac3b1e74e916d278a2
Christopher <mwanikigachina@gmail.com>
2024-01-12 19:56:16 +0300

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+    {
+        name: "Bob Marley - Waiting In Vain",
+        artist: "Bob Marley",
+        img: "image1",
+        src: "Waiting In Vain"
+    },
+
+    {
+        name: "Bob Marley - Three Little Birds",
+        artist: "Bob Marley",
+        img: "image2",
+        src: "Three Little Birds"
+    },
+    {
+        name: "Bob Marley - One Love",
+        artist: "Bob Marley",
+        img: "image3",
+        src: "One Love"
+    },
+    {
+        name: "Bob Marley - Rastaman Live Up",
+        artist: "Bob Marley",
+        img: "image4",
+        src: "Rastaman Live Up"
+    },
+    {
+        name: "Bob Marley - Redemption Song",
+        artist: "Bob Marley",
+        img: "image5",
+        src: "Redemption Song"
+        },
+    {
+        name: "Bob Marley - No Women, No Cry",
+        artist: "Bob Marley",
+        img: "image6",
+        src: "No women, no cry"
+    }
+]
